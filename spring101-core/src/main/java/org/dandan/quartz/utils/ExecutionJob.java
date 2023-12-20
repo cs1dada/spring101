fafab4fa21b0946e7b435434def2c274f7c186bf
dandan <cse3dadakiller@gmail.com>
2023-12-20 10:58:55 +0800

    QuartzManage.java 新增刪除修改Job到scheduler
    ExecutionJob.java
    封裝QuartzJobBean, 實際要執行的job邏輯藉由QuartzRunnable取出
    
    QuartzRunnable.java
    將 Quartz 任務中執行的業務邏輯封装为一個可调用的任務，便于在 Quartz 中调度執行。

---
+++ spring101-core/src/main/java/org/dandan/quartz/utils/ExecutionJob.java
@@ -0,0 +1,92 @@
+package org.dandan.quartz.utils;
+
+import org.dandan.quartz.domain.SysQuartzJob;
+import org.dandan.quartz.domain.SysQuartzLog;
+import org.dandan.quartz.repository.SysQuartzLogRepository;
+import org.dandan.quartz.service.SysQuartzJobService;
+import org.dandan.utils.SpringContextHolder;
+import org.dandan.utils.StringUtils;
+import org.dandan.utils.ThrowableUtil;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.concurrent.Future;
+
+@Async
+public class ExecutionJob extends QuartzJobBean {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final ThreadPoolTaskExecutor executor = SpringContextHolder.getBean("elAsync");
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        logger.info("ExecutionJob executeInternal !!!");
+        SysQuartzJob quartzJob = (SysQuartzJob) context.getMergedJobDataMap().get(SysQuartzJob.JOB_KEY);
+        // 获取spring bean
+        SysQuartzLogRepository quartzLogRepository = SpringContextHolder.getBean(SysQuartzLogRepository.class);
+        SysQuartzJobService quartzJobService = SpringContextHolder.getBean(SysQuartzJobService.class);
+        //RedisUtils redisUtils = SpringContextHolder.getBean(RedisUtils.class);
+
+        String uuid = quartzJob.getUuid();
+
+        SysQuartzLog log = new SysQuartzLog();
+        log.setJobName(quartzJob.getJobName());
+        log.setBeanName(quartzJob.getBeanName());
+        log.setMethodName(quartzJob.getMethodName());
+        log.setParams(quartzJob.getParams());
+        long startTime = System.currentTimeMillis();
+        log.setCronExpression(quartzJob.getCronExpression());
+
+        try {
+            // 执行任务
+            QuartzRunnable task = new QuartzRunnable(quartzJob.getBeanName(), quartzJob.getMethodName(), quartzJob.getParams());
+            Future<?> future = executor.submit(task);
+            future.get();
+            long times = System.currentTimeMillis() - startTime;
+            log.setTime(times);
+            if(StringUtils.isNotBlank(uuid)) {
+                //redisUtils.set(uuid, true);
+            }
+            // 任务状态
+            log.setSuccess(true);
+            logger.info("任务执行成功，任务名称：" + quartzJob.getJobName() + ", 执行时间：" + times + "毫秒");
+            // 判断是否存在子任务
+            if(StringUtils.isNotBlank(quartzJob.getSubTask())){
+                String[] tasks = quartzJob.getSubTask().split("[,，]");
+                // 执行子任务
+                //quartzJobService.executionSubJob(tasks);
+            }
+        } catch (Exception e) {
+            if(StringUtils.isNotBlank(uuid)) {
+                //redisUtils.set(uuid, false);
+            }
+            logger.error("任务执行失败，任务名称：" + quartzJob.getJobName());
+            long times = System.currentTimeMillis() - startTime;
+            log.setTime(times);
+            // 任务状态 0：成功 1：失败
+            log.setSuccess(false);
+            log.setExceptionDetail(ThrowableUtil.getStackTrace(e));
+            // 任务如果失败了则暂停
+            if(quartzJob.getPauseAfterFailure() != null && quartzJob.getPauseAfterFailure()){
+                quartzJob.setPause(false);
+                //更新状态
+                //quartzJobService.updateIsPause(quartzJob);
+            }
+            if(quartzJob.getEmail() != null) {
+//                EmailService emailService = SpringContextHolder.getBean(EmailService.class);
+//                // 邮箱报警
+//                if(StringUtils.isNoneBlank(quartzJob.getEmail())){
+//                    EmailVo emailVo = taskAlarm(quartzJob, ThrowableUtil.getStackTrace(e));
+//                    emailService.send(emailVo, emailService.find());
+//                }
+            }
+        } finally {
+            quartzLogRepository.save(log);
+        }
+    }
+}
---
+++ spring101-core/src/main/java/org/dandan/quartz/utils/QuartzManage.java
@@ -0,0 +1,119 @@
+package org.dandan.quartz.utils;
+
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.dandan.exception.BadRequestException;
+import org.dandan.quartz.domain.SysQuartzJob;
+import org.quartz.*;
+import org.quartz.impl.matchers.GroupMatcher;
+import org.quartz.impl.triggers.CronTriggerImpl;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.quartz.TriggerBuilder.newTrigger;
+
+@Slf4j
+@Component
+public class QuartzManage {
+    private static final String JOB_NAME = "TASK_";
+
+    @Resource
+    private Scheduler scheduler;
+
+    /**
+     * 新增job到quartz裡面, 準備執行
+     * @param quartzJob
+     */
+    public void addJob(SysQuartzJob quartzJob){
+        try {
+            // 构建job信息
+            JobDetail jobDetail = JobBuilder.newJob(ExecutionJob.class).
+                    withIdentity(JOB_NAME + quartzJob.getJobId(), "dandan").build();
+
+            //通过触发器名和cron 表达式创建 Trigger
+            Trigger cronTrigger = newTrigger()
+                    .withIdentity(JOB_NAME + quartzJob.getJobId(), "dandan")
+                    .startNow()
+                    .withSchedule(CronScheduleBuilder.cronSchedule(quartzJob.getCronExpression()))
+                    .build();
+
+            cronTrigger.getJobDataMap().put(SysQuartzJob.JOB_KEY, quartzJob);
+
+            //重置启动时间
+            ((CronTriggerImpl)cronTrigger).setStartTime(new Date());
+
+            //执行定时任务
+            scheduler.scheduleJob(jobDetail,cronTrigger);
+
+            // 暂停任务
+            if (quartzJob.getPause()) {
+                pauseJob(quartzJob);
+            }
+        } catch (Exception e){
+            log.error("创建定时任务失败", e);
+            throw new BadRequestException("创建定时任务失败");
+        }
+    }
+
+
+    /**
+     * 立即执行job
+     * @param quartzJob /
+     */
+    public void runJobNow(SysQuartzJob quartzJob){
+        try {
+            TriggerKey triggerKey = TriggerKey.triggerKey(JOB_NAME + quartzJob.getJobId(), "dandan");
+            CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
+            // 如果不存在则创建一个定时任务
+            if(trigger == null) {
+                addJob(quartzJob);
+            }
+            JobDataMap dataMap = new JobDataMap();
+            dataMap.put(SysQuartzJob.JOB_KEY, quartzJob);
+            JobKey jobKey = JobKey.jobKey(JOB_NAME + quartzJob.getJobId(), "dandan");
+            scheduler.triggerJob(jobKey,dataMap);
+        } catch (Exception e){
+            log.error("定时任务执行失败", e);
+            throw new BadRequestException("定时任务执行失败");
+        }
+    }
+    /**
+     * 暂停一个job
+     * @param quartzJob /
+     */
+    public void pauseJob(SysQuartzJob quartzJob){
+        try {
+            JobKey jobKey = JobKey.jobKey(JOB_NAME + quartzJob.getJobId(), "dandan");
+            scheduler.pauseJob(jobKey);
+        } catch (Exception e){
+            log.error("定时任务暂停失败", e);
+            throw new BadRequestException("定时任务暂停失败");
+        }
+    }
+
+    public List<JobExecutionContext> getCurrentlyExecutingJobs() throws SchedulerException {
+        return scheduler.getCurrentlyExecutingJobs();
+    }
+
+    public List<JobKey> getJobKeys() throws SchedulerException {
+        Set<JobKey> jobKeys = scheduler.getJobKeys(GroupMatcher.anyGroup());
+        return new ArrayList<>(jobKeys);
+    }
+
+    public List<JobDetail> getJobDetails() throws SchedulerException {
+        Set<JobKey> jobKeys = scheduler.getJobKeys(GroupMatcher.anyGroup());
+        List<JobDetail> list = new ArrayList<>();
+        for (JobKey e : jobKeys) {
+            JobDetail jobDetail = scheduler.getJobDetail(e);
+            list.add(jobDetail);
+        }
+        return list;
+
+    }
+}
